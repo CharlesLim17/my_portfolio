b2dfc388a826b498c6416bcebd1e12d2f200b249
Charles Angelo Lim <choylim06@gmail.com>
2023-06-13 19:49:08 +0800

    tried to make nav responsive

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,3 +11,14 @@ menuLinks.forEach(link => {
     });
   });
 });
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleNavFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
