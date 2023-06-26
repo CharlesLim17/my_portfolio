b2ad09b0852fc60ccaabe96cab50f0a5a12fb5bc
Charles Angelo Lim <choylim06@gmail.com>
2023-06-27 00:36:15 +0800

    project 1 done

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,7 +14,7 @@ menuLinks.forEach(link => {
 
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function toggleNavFunction() {
+function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
